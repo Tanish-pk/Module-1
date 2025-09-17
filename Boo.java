623c1f04939279c503665535fd0c7872c6e83e29
Tanish-pk <thanish.pk@gmail.com>
2025-09-17 15:28:47 +0530

    Add files via upload

---
+++ Boo.java
@@ -0,0 +1,15 @@
+public class Boo {
+    public static void main(String[] args) {
+        int a=5;
+        int b=-5;
+
+        if(b>0) {
+            System.out.println("the number is positive  ");
+
+        }
+
+        else {
+            System.out.println("the number is negative ");
+        }
+    }
+}
---
+++ palindrome.java
@@ -0,0 +1,11 @@
+//take a input and check if its palindrome
+import java.util.*;
+public class palindrome {
+    public static void main(String[] args) {
+        String s;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string : ");
+        s=sc.nextLine();
+        
+    }
+}
