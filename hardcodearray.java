da090ba5f85602d81ab501a876848b0fe91330e6
Tanish-pk <thanish.pk@gmail.com>
2025-09-17 15:31:21 +0530

    Add files via upload

---
+++ hardcodearray.java
@@ -0,0 +1,14 @@
+
+public class hardcodearray {
+    public static void main(String[] args){
+        int arr[]={5,10,15,20,25,30,35,40,45,50};
+
+        for(int i =0;i<10;i++) {
+            System.out.println("Element at index "+i+" :  "+arr[i]);
+        }
+
+
+        
+    }
+    
+}
